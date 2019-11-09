d783cb01697d37bb18725904fa4ac4be2b4362c4
None <None>
2019-11-09 08:08:41 -0800

    fixed bug

--- mininetTopo.py
+++ mininetTopo.py
@@ -11,43 +11,52 @@ from mininet.log import setLogLevel, info
 from mininet.cli import CLI
 from mininet.topo import Topo
 from mininet.link import Link
+from mininet.link import TCLink
+
 from mininet.node import RemoteController
 
 net = None
 
 class TreeTopo(Topo):
 			
-	def __init__(self):
+    def __init__(self):
 		# Initialize topology
-		Topo.__init__(self)        
+	Topo.__init__(self)        
 	
 	# You can write other functions as you need.
 
 	# Add hosts
-    # > self.addHost('h%d' % [HOST NUMBER])
-    for i in (1, 8):
-        self.addHost('h%d' % [i])
+    	# > self.addHost('h%d' % [HOST NUMBER])
+    	h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
+        h3 = self.addHost('h3')
+        h4 = self.addHost('h4')
+        h5 = self.addHost('h5')
+        h6 = self.addHost('h6')
+        h7 = self.addHost('h7')
 
 	# Add switches
-    for i in (1, 5):
-        self.addSwitch('s{}' % [i]))
-    # > sconfig = {'dpid': "%016x" % [SWITCH NUMBER]}
-    # > self.addSwitch('s%d' % [SWITCH NUMBER], **sconfig)
+    	s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+        s3 = self.addSwitch('s3')
+        s4 = self.addSwitch('s4')
+    	# > sconfig = {'dpid': "%016x" % [SWITCH NUMBER]}
+    	# > self.addSwitch('s%d' % [SWITCH NUMBER], **sconfig)
 
     
 	# Add links
 	# > self.addLink([HOST1], [HOST2])
-    self.addLink(h1, s1, bw=10)
-    self.addLink(h2, s1, bw=10)
-    self.addLink(h3, s2, bw=10)
-    self.addLink(h4, s2, bw=10)
-    self.addLink(h5, s3, bw=10)
-    self.addLink(h6, s3, bw=10)
-    self.addLink(h7, s3, bw=10)
-    self.addLink(s1, s2, bw=100)
-    self.addLink(s2, s3, bw=100)
-    self.addLink(s3, s4, bw=100)
-    self.addLink(s1, s4, bw=100)
+   	self.addLink(h1, s1, bw=10)
+    	self.addLink(h2, s1, bw=10)
+    	self.addLink(h3, s2, bw=10)
+    	self.addLink(h4, s2, bw=10)
+    	self.addLink(h5, s3, bw=10)
+    	self.addLink(h6, s3, bw=10)
+    	self.addLink(h7, s3, bw=10)
+    	self.addLink(s1, s2, bw=100)
+    	self.addLink(s2, s3, bw=100)
+    	self.addLink(s3, s4, bw=100)
+    	self.addLink(s1, s4, bw=100)
 
 
 def startNetwork():
@@ -55,13 +64,13 @@ def startNetwork():
     topo = TreeTopo()
 
     global net
-    net = Mininet(topo=topo, link = Link,
+    net = Mininet(topo=topo, link = TCLink,
                   controller=lambda name: RemoteController(name, ip='SERVER IP'),
                   listenPort=6633, autoSetMacs=True)
 
     info('** Starting the network\n')
     net.start()
-
+    #net.pingAll()
     # Create QoS Queues
     # > os.system('sudo ovs-vsctl -- set Port [INTERFACE] qos=@newqos \
     #            -- --id=@newqos create QoS type=linux-htb other-config:max-rate=[LINK SPEED] queues=0=@q0,1=@q1,2=@q2 \
